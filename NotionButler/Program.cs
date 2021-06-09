2a5e97ba7dec25887abaabb1513c957d1033635e
Artem Sevostianov <artem.sevos@gmail.com>
2021-06-09 03:38:56 +0300

    ♻️ Refactores almost to production ready state

--- NotionButler/Program.cs
+++ NotionButler/Program.cs
@@ -7,39 +7,48 @@ namespace NotionButler
     {
         static async Task Main(string[] args)
         {
+            // Fetching env variables
+
             DotNetEnv.Env.Load("../prod.env");
             var botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
             var ownerId = long.Parse(Environment.GetEnvironmentVariable("TELEGRAM_ID"));
             var notionToken = Environment.GetEnvironmentVariable("NOTION_TOKEN");
             var tasksDbId = Environment.GetEnvironmentVariable("TASKS_DB_ID");
+            // TODO - make env variable for this?
+            var fetchTasksTime = TimeSpan.FromMinutes(3 * 60 + 38);
+
+            // Instanciating clients
 
             var notion = new NotionWorker(notionToken, tasksDbId);
             var telegram = new TelegramWorker(botToken, ownerId, notion);
 
-            var fetchInboxTask = notion.FetchInboxTodos();
-            var fetchCurrentTodosTask = notion.FetchCurrentTodos();
+            // Main working loop
 
-            await Task.WhenAll(fetchInboxTask, fetchCurrentTodosTask);
-            if (fetchCurrentTodosTask.Result.Count > 0)
+            while (true)
             {
-                var resultMessage = "Доброе утро! Сегодняшние дела:";
-                fetchCurrentTodosTask.Result.ForEach(page =>
-                {
-                    var todoTitle = Utils.GetTodoTitle(page);
-                    resultMessage += $"\n- {todoTitle}";
-                });
+                var waitTime = Utils.CalcTimeToNextFetch(fetchTasksTime);
+                Console.WriteLine($"Next fetch in {waitTime.ToString()}");
+                await Task.Delay(waitTime);
 
-                var inboxCount = fetchInboxTask.Result.Count;
-                if (inboxCount > 0)
+                var fetchInboxTask = notion.FetchInboxTodos();
+                var fetchCurrentTodosTask = notion.FetchCurrentTodos();
+                await Task.WhenAll(fetchInboxTask, fetchCurrentTodosTask);
+
+                var currentTodos = fetchCurrentTodosTask.Result;
+                var inbox = fetchInboxTask.Result;
+
+                if (currentTodos.Count > 0)
                 {
-                    resultMessage += $"\nЗадач в инбоксе: {inboxCount}";
+                    var resultMessage = "Доброе утро! Сегодняшние дела:";
+                    resultMessage += Utils.GetAllTitlesAsBulletedList(currentTodos);
+                    if (inbox.Count > 0)
+                    {
+                        resultMessage += $"\nЗадач в инбоксе: {inbox.Count}";
+                    }
+
+                    await telegram.SendMessageToOwner(resultMessage);
                 }
-
-                await telegram.SendMessageToOwner(resultMessage);
             }
-
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
         }
     }
 }
--- NotionButler/Utils.cs
+++ NotionButler/Utils.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Notion.Client;
 
 namespace NotionButler
@@ -10,5 +13,24 @@ namespace NotionButler
             page.Properties.TryGetValue("Что", out title);
             return ((TitlePropertyValue)title).Title[0].PlainText;
         }
+
+        public static string GetAllTitlesAsBulletedList(List<RetrievedPage> pages)
+        {
+            return pages.Aggregate("", (result, page) => result += $"\n- {GetTodoTitle(page)}");
+        }
+
+        public static TimeSpan CalcTimeToNextFetch(TimeSpan checkTime)
+        {
+            var timeOfToday = DateTime.Now - DateTime.Today;
+            if (timeOfToday <= checkTime)
+            {
+                return checkTime - timeOfToday;
+            }
+            else
+            {
+                return DateTime.Today.AddDays(1) - DateTime.Now + checkTime;
+            }
+        }
+
     }
 }

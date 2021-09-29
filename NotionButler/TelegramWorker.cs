ebc33c94d93baef16162935047725f43dbba6186
Artem Sevostianov <artem.sevos@gmail.com>
2021-09-29 15:42:30 +0300

    ✨ Reworked TelegramWorker to process random users messages

--- NotionButler/TelegramWorker.cs
+++ NotionButler/TelegramWorker.cs
@@ -24,31 +24,55 @@ namespace NotionButler
 
         private void SetOnMessageBehavior()
         {
-            _botClient.OnMessage += OnOwnerMessage;
-            _botClient.OnMessage += OnOtherMessage;
+            _botClient.OnMessage += OnMessage;
         }
 
-        private async void OnOwnerMessage(object sender, MessageEventArgs e)
+        private async void OnMessage(object sender, MessageEventArgs e)
         {
-            if (e.Message.Chat.Id == _ownerId)
+            if (e.Message.Text == "/start")
             {
-                await _notionClient.AddTodoToInbox(e.Message.Text);
-                await SendMessageToOwner($"Добавил в инбокс 👌");
+                await OnStartMessage(sender, e);
+            }
+            else if (e.Message.Chat.Id == _ownerId)
+            {
+                await AddTodoToInbox(sender, e);
+            }
+            else
+            {
+                await ProcessFeedback(sender, e);
             }
         }
 
-        private async void OnOtherMessage(object sender, MessageEventArgs e)
+        private async Task OnStartMessage(object sender, MessageEventArgs e)
         {
-            // TODO: Add suggestions
-            if (e.Message.Chat.Id != _ownerId)
+            if (e.Message.From.Id == _ownerId)
             {
-                await _botClient.SendTextMessageAsync(e.Message.Chat.Id, "Я тебя не знаю");
+                await SendMessageToOwner($"Ты знаешь, что делать");
             }
+            else
+            {
+                var greetingMsg = "This bot does not have publicly available features yet.\n\n"
+                    + "If you have any suggestions for Notion Telegram Bot features please send "
+                    + "them to the bot";
+                await _botClient.SendTextMessageAsync(e.Message.Chat.Id, greetingMsg);
+            }
+        }
+
+        private async Task AddTodoToInbox(object sender, MessageEventArgs e)
+        {
+            await _notionClient.AddTodoToInbox(e.Message.Text);
+            await SendMessageToOwner($"Добавил в инбокс 👌");
         }
 
         public async Task SendMessageToOwner(string message)
         {
             await _botClient.SendTextMessageAsync(_ownerId, message);
+        }
+
+        private async Task ProcessFeedback(object sender, MessageEventArgs e)
+        {
+            await _botClient.ForwardMessageAsync(_ownerId, e.Message.From.Id, e.Message.MessageId);
+            await _botClient.SendTextMessageAsync(e.Message.From.Id, "Thank you for your suggestion!");
         }
     }
 }
